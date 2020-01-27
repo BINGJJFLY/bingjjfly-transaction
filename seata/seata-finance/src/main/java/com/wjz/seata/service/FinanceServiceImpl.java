95a3333327ab848f32caf6510c538a8cb4a3b130
wangjz <17091267797@163.com>
2020-01-27 22:21:55 +0800

    Finance添加本地事务及测试异常情况

--- seata/seata-finance/src/main/java/com/wjz/seata/service/FinanceServiceImpl.java
+++ seata/seata-finance/src/main/java/com/wjz/seata/service/FinanceServiceImpl.java
@@ -1,6 +1,7 @@
 package com.wjz.seata.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.wjz.seata.domain.Finance;
@@ -13,9 +14,11 @@ public class FinanceServiceImpl implements FinanceService {
 	private FinanceMapper mapper;
 
 	@Override
+	@Transactional
 	public void insert(String name) {
 		Finance finance = new Finance();
 		finance.setName(name);
 		mapper.insert(finance);
+//		int i = 1 / 0;
 	}
 }
